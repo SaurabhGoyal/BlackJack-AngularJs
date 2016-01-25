5d621935d13745e927ab5bdc4c117874be5f45c2
Saurabh Goyal <saurabh.2561@gmail.com>
2016-01-25 09:39:40 +0530

    add requirements, improve html

--- tic_tac_toe_ng/assets/scripts/config.js
+++
@@ -1,6 +0,0 @@
-TicTacToeNg.config(
-    function($interpolateProvider) {
-        $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
-        // For preventing conflict with django template rendering
-    }
-);
